315dfbcafd7fa6dcc2c9e9fc34c8fa86c505135f
Thanh <Tbaho4113@gmail.com>
2025-05-23 15:50:37 -0400

    test2

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 #define onboard 13
 
 void setup() {
-
+  pinMode(onboard, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(onboard, HIGH);
+  delay(1000);
+  digitalWrite(onboard, LOW);
+  delay(1000);
 }
