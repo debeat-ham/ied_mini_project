9c4825b5e5b5b769fd24aafec8361fd1bb63d68a
Cori DeBeatham <cdebeatham@gmail.com>
2025-05-24 00:42:25 -0400

    Initialized sensor reading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,52 @@
 #include <Arduino.h>
+
 #define onboard 13
 
+//Analog Sensor Pins
+int S1 = A1;
+int S2 = A2;
+int S3 = A3;
+int S4 = A4;
+int S5 = A5;
+
+//Function to read sensors, display results in serial monitor and return array of sensor values.
+int* read_sensors(){
+    int* sensor_array = new int[5];
+
+    sensor_array[0] = analogRead(S1);
+    sensor_array[1] = analogRead(S2);
+    sensor_array[2] = analogRead(S3);
+    sensor_array[3] = analogRead(S4);
+    sensor_array[4] = analogRead(S5);
+
+    Serial.print("Sensor Readings: ");
+    Serial.print(sensor_array[0]);
+    Serial.print(" ");
+    Serial.print(sensor_array[1]);
+    Serial.print(" ");
+    Serial.print(sensor_array[2]);
+    Serial.print(" ");
+    Serial.print(sensor_array[3]);
+    Serial.print(" ");
+    Serial.println(sensor_array[4]);
+
+    return sensor_array;
+}
+
 void setup() {
-  pinMode(onboard, OUTPUT);
+  //Define sensor array as an input
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
+
+  //Initialize serial monitor
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(onboard, HIGH);
-  delay(1000);
-  digitalWrite(onboard, LOW);
-  delay(1000);
+  //Code to continuously display sensor readings
+  int* sensor_array = read_sensors();
+  delay(200);
 }
