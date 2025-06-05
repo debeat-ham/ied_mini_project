73670deda3b44553d6f02f8257bc878fa15b9b46
Jeremy Goldberger <87028711+jgoldberger26@users.noreply.github.com>
2025-06-05 12:56:29 -0400

    Make speed and base_factor tunable

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,10 @@
 #include "pid.h"
 
 // PID parameters
-const float kp = 0.1;
+const float kp = 0.3; // Measured during testing, can retest
 const float ki = 0;
 const float kd = 0;
 
-const float speed = -1;
 
 pid_controller controller(kp, ki, kd);
 
@@ -21,9 +20,22 @@ void setup() {
 }
 
 void loop() {
+  /* =================== Tunable Constants =================== */
+  /* Note: Only one constant is tunable at a time,
+   * i.e. you can only have one get_potentiometer uncommented. */
+  
+   // float p_val = get_potentiometer() * 0.3f; // Can be made higher if base_factor is lower
+  float p_val = kp;
+  
+  // float base_factor = 0.5f;
+  float base_factor = get_potentiometer();
+
+  // float speed = get_potentiometer() * -1;
+  float speed = -1;
+
+  /* ================= End Tunable Constants ================= */
   float error = get_error();
-  float p_val = get_potentiometer() * 0.3f;
-  float turn_factor = 0.5f + controller.execute(error, p_val);
+  float turn_factor = base_factor + controller.execute(error, p_val);
   
   if (car_enabled()) {
     set_left_motor((1- turn_factor) * speed);
@@ -35,10 +47,10 @@ void loop() {
   
   Serial.print("P value: ");
   Serial.print(p_val);
-  // Serial.print("\tLeft motor: ");
-  // Serial.print(1 - turn_factor);
-  // Serial.print("\tRight motor: ");
-  // Serial.print(turn_factor);
+  Serial.print("\tSpeed: ");
+  Serial.print(speed);
+  Serial.print("\tBase factor");
+  Serial.print(base_factor);
   Serial.println();
 
   // The PID mus tbe evaluated at a regular interval, so we delay a little.
